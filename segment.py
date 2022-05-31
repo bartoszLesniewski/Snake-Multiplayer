d30c42ca2148dc1a6313f7b471ad470d85f94e3d
Bartosz Leśniewski <blesniewski@outlook.com>
2022-05-31 14:25:30 +0200

    Final version

--- segment.py
+++ segment.py
@@ -1,9 +1,11 @@
 import pygame
 
+from constans import GREEN
+
 
 class Segment(pygame.sprite.Sprite):
     def __init__(self, x_position, y_position):
         super().__init__()
         self.surface = pygame.Surface((20, 20))
-        self.surface.fill((0, 100, 0))
+        self.surface.fill(GREEN)
         self.rect = self.surface.get_rect(x=x_position, y=y_position)
